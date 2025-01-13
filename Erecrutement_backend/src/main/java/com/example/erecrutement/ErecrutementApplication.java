d4516061fa49751ddbc90341e6327f8720879576
Dhia Eddinne Sbei <100136460+DhiaDEV@users.noreply.github.com>
2025-01-13 19:03:19 +0100

    initial commit

---
+++ Erecrutement_backend/src/main/java/com/example/erecrutement/ErecrutementApplication.java
@@ -0,0 +1,13 @@
+package com.example.erecrutement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErecrutementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErecrutementApplication.class, args);
+    }
+
+}
---
+++ Erecrutement_backend/src/test/java/com/example/erecrutement/ErecrutementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erecrutement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErecrutementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
